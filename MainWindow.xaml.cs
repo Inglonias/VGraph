acd55194a921dec1cb3d0744892375ff921e511e
Inglonias <2125926+Inglonias@users.noreply.github.com>
2021-09-08 18:48:41 -0400

    Some changes and stuff
    * Refactored grid-specific methods to their own class
    * Capitalized all the methods and variables. I came from Java, where we don't do that and it's something I need to get used to
    * Fixed all regressions points 1 and 2 caused.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,61 +21,30 @@ namespace VGraph
     /// </summary>
     public partial class MainWindow : Window
     {
-        //Default values are best estimates for 1/4 inch squares on 8.5 x 11.
-        int squareWidth = 30;
-        int squareHeight = 39;
-        int squareSize = 24;
-        int margin = 48;
+        int DotX = 0;
+        int DotY = 0;
 
-        int xSize = 0;
-        int ySize = 0;
-        int dotX = 0;
-        int dotY = 0;
+        bool ClickedOnce = false;
 
-        SKBitmap gridMap;
-        SKBitmap dotMap;
+        //SKBitmap gridMap;
+        readonly GridBackground Grid;
+        SKBitmap DotMap;
 
         public MainWindow()
         {
-            gridMap = generateGridBitmap(squareWidth, squareHeight, squareSize);
+            //Default values are best estimates for 1/4 inch squares on 8.5 x 11.
+            Grid = new GridBackground(30, 39, 24, 48);
             InitializeComponent();
-            mainCanvas.Width = gridMap.Width;
-            mainCanvas.Height = gridMap.Height;
-            InvalidateVisual();
+            Grid.GetGridBitmap();
+            MainCanvas.Width = Grid.GetTotalWidth();
+            MainCanvas.Height = Grid.GetTotalHeight();
         }
 
-        private SKBitmap generateGridBitmap(int squaresWide, int squaresTall, int squareSize)
-        {
-            xSize = ((squaresWide * squareSize) + (margin * 2));
-            ySize = ((squaresTall * squareSize) + (margin * 2));
-
-            SKBitmap grid = new SKBitmap(xSize, ySize);
-
-            //Disposables
-            SKCanvas gridCanvas = new SKCanvas(grid);
-            SKPaint brush = new SKPaint { Style = SKPaintStyle.Stroke, StrokeWidth = 1 };
-
-            for (int x = 0; x < squaresWide; x++)
-            {
-                for (int y = 0; y < squaresTall; y++)
-                {
-                    int xStart = (x * squareSize) + margin;
-                    int yStart = (y * squareSize) + margin;
-                    SKRectI squareToDraw = new SKRectI(xStart, yStart, xStart + squareSize, yStart + squareSize);
-                    gridCanvas.DrawRect(squareToDraw, brush);
-                }
-            }
-
-            //Dispose of them.
-            gridCanvas.Dispose();
-            brush.Dispose();
-
-            return grid;
-        }
+        
 
         private SKBitmap generateDotBitmap(int dotX, int dotY)
         {
-            SKBitmap dotMap = new SKBitmap(xSize, ySize);
+            SKBitmap dotMap = new SKBitmap(Grid.GetTotalWidth(), Grid.GetTotalHeight());
 
             //Disposables
             SKCanvas dotCanvas = new SKCanvas(dotMap);
@@ -90,65 +59,56 @@ namespace VGraph
             return dotMap;
         }
 
-        private SKPointI roundToNearestIntersection(int x, int y)
-        {
-            //Subtract the margin out.
-            int mouseX = x - margin;
-            int mouseY = y - margin;
-
-            //Round to the nearest intersection.
-            int targetX = ((mouseX % squareSize) < (squareSize / 2)) ?
-                    (mouseX - (mouseX % squareSize)) :
-                    (mouseX + (squareSize - (mouseX % squareSize)));
-            int targetY = ((mouseY % squareSize) < (squareSize / 2)) ?
-                    (mouseY - (mouseY % squareSize)) :
-                    (mouseY + (squareSize - (mouseY % squareSize)));
-
-            //Add the margin back in.
-            targetX += margin;
-            targetY += margin;
-
-            return new SKPointI(targetX, targetY);
-        }
-        private SKPointI roundToNearestIntersection(SKPointI coords)
-        {
-            return roundToNearestIntersection(coords.X, coords.Y);
-        }
-
         private void MainCanvas_OnMouseMove(object sender, MouseEventArgs e)
         {
-            int mouseX = Convert.ToInt32(e.GetPosition(mainCanvas).X);
-            int mouseY = Convert.ToInt32(e.GetPosition(mainCanvas).Y);
+            SKPointI targetCoords = Grid.RoundToNearestIntersection(e.GetPosition(MainCanvas));
+            int mouseX = targetCoords.X;
+            int mouseY = targetCoords.Y;
 
-            if ((mouseX - margin < 0) || mouseY - margin < 0 || mouseX + margin > mainCanvas.Width || mouseY + margin > mainCanvas.Height)
+            if ((mouseX - Grid.GetMargin() < 0) || mouseY - Grid.GetMargin() < 0 || mouseX + Grid.GetMargin() > MainCanvas.Width || mouseY + Grid.GetMargin() > MainCanvas.Height)
             {
                 //Ignore it if we're out of bounds.
                 return;
             }
-            SKPointI targetCoords = roundToNearestIntersection(mouseX, mouseY);
+            
 
             //Don't re-render if there wasn't actually any sort of change made.
-            if (targetCoords.X != dotX || targetCoords.Y != dotY)
+            if (targetCoords.X != DotX || targetCoords.Y != DotY)
             {
-                if (dotMap != null)
+                if (DotMap != null)
                 {
                     //There's a leak if we don't do this.
-                    dotMap.Dispose();
+                    DotMap.Dispose();
                 }
-                dotMap = generateDotBitmap(targetCoords.X, targetCoords.Y);
-                dotX = targetCoords.X;
-                dotY = targetCoords.Y;
-                mainCanvas.InvalidateVisual();
+                DotMap = generateDotBitmap(targetCoords.X, targetCoords.Y);
+                DotX = targetCoords.X;
+                DotY = targetCoords.Y;
+                MainCanvas.InvalidateVisual();
             }
         }
 
         private void MainCanvas_OnPaintSurface(object sender, SkiaSharp.Views.Desktop.SKPaintSurfaceEventArgs e)
         {
             e.Surface.Canvas.Clear(SKColors.White);
-            e.Surface.Canvas.DrawBitmap(gridMap, 0, 0);
-            if (dotMap != null)
+            e.Surface.Canvas.DrawBitmap(Grid.GetGridBitmap(), 0, 0);
+            if (DotMap != null)
             {
-                e.Surface.Canvas.DrawBitmap(dotMap, 0, 0);
+                e.Surface.Canvas.DrawBitmap(DotMap, 0, 0);
+            }
+        }
+
+        private void MainCanvas_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            SKPointI target = Grid.RoundToNearestIntersection(e.GetPosition(MainCanvas));
+            if (!ClickedOnce)
+            {
+                ClickedOnce = true;
+                Console.WriteLine("Click at X: " + target.X + " Y: " + target.Y);
+            }
+            else
+            {
+                ClickedOnce = false;
+                Console.WriteLine("Click again at X: " + target.X + " Y: " + target.Y);
             }
         }
     }
