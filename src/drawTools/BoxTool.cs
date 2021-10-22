4d3410534b8036050519bda604194ab39b1c979c
Inglonias <2125926+Inglonias@users.noreply.github.com>
2021-10-22 11:32:13 -0400

    Made Box tool functional

--- src/drawTools/BoxTool.cs
+++ src/drawTools/BoxTool.cs
@@ -7,6 +7,16 @@ public class BoxTool : IDrawTool
 {
     public LineSegment[] DrawWithTool(SKPointI start, SKPointI end)
     {
-        
+        LineSegment[] rVal = new LineSegment[4];
+
+        SKPointI topRightCorner = new SKPointI(end.X, start.Y);
+        SKPointI bottomLeftCorner = new SKPointI(start.X, end.Y);
+
+        rVal[0] = new LineSegment(start, topRightCorner);
+        rVal[1] = new LineSegment(topRightCorner, end);
+        rVal[2] = new LineSegment(end, bottomLeftCorner);
+        rVal[3] = new LineSegment(bottomLeftCorner, start);
+
+        return rVal;
     }
 }
