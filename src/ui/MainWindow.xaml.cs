fb60fc4a20670aedbb2d0c0e29e3d70dcfc5ef9c
Inglonias <2125926+Inglonias@users.noreply.github.com>
2021-12-30 13:40:35 -0500

    More refactoring and code cleanup.

--- src/ui/MainWindow.xaml.cs
+++ src/ui/MainWindow.xaml.cs
@@ -151,48 +151,5 @@ namespace VGraph.src.ui
         {
             MainCanvas.InvalidateVisual();
         }
-
-        private void NewCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            MainMenuBar.CreateNewGrid();
-        }
-
-        private void OpenCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            MainMenuBar.OpenGrid();
-        }
-
-        private void SaveCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            MainMenuBar.SaveGrid();
-        }
-
-        private void ExportCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            MainMenuBar.ExportGrid();
-        }
-
-        private void ExitCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            MainMenuBar.ExitApp();
-        }
-    }
-
-    public class MenuCommands
-    {
-        //File Menu
-        public static RoutedCommand NewGridCmd = new RoutedCommand("NewGridCmd", typeof(MenuCommands));
-        public static RoutedCommand OpenGridCmd = new RoutedCommand("OpenGridCmd", typeof(MenuCommands));
-        public static RoutedCommand SaveGridCmd = new RoutedCommand("SaveGridCmd", typeof(MenuCommands));
-        public static RoutedCommand ExportGridCmd = new RoutedCommand("ExportGridCmd", typeof(MenuCommands));
-        public static RoutedCommand ExitCmd = new RoutedCommand("ExitCmd", typeof(MenuCommands));
-
-        //Edit Menu
-        public static RoutedCommand UndoCmd = new RoutedCommand("UndoCmd", typeof(MenuCommands));
-        public static RoutedCommand RedoCmd = new RoutedCommand("RedoCmd", typeof(MenuCommands));
-
-        //View Menu
-
-        //Tools Menu
     }
 }
---
+++ src/ui/MainWindowCommands.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace VGraph.src.ui
+{
+    public partial class MainWindow : Window
+    {
+        private void NewCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MainMenuBar.CreateNewGrid();
+        }
+
+        private void OpenCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MainMenuBar.OpenGrid();
+        }
+
+        private void SaveCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MainMenuBar.SaveGrid();
+        }
+
+        private void ExportCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MainMenuBar.ExportGrid();
+        }
+
+        private void ExitCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MainMenuBar.ExitApp();
+        }
+    }
+    public class MenuCommands
+    {
+        //File Menu
+        public static RoutedCommand NewGridCmd = new RoutedCommand("NewGridCmd", typeof(MenuCommands));
+        public static RoutedCommand OpenGridCmd = new RoutedCommand("OpenGridCmd", typeof(MenuCommands));
+        public static RoutedCommand SaveGridCmd = new RoutedCommand("SaveGridCmd", typeof(MenuCommands));
+        public static RoutedCommand ExportGridCmd = new RoutedCommand("ExportGridCmd", typeof(MenuCommands));
+        public static RoutedCommand ExitCmd = new RoutedCommand("ExitCmd", typeof(MenuCommands));
+
+        //Edit Menu
+        public static RoutedCommand UndoCmd = new RoutedCommand("UndoCmd", typeof(MenuCommands));
+        public static RoutedCommand RedoCmd = new RoutedCommand("RedoCmd", typeof(MenuCommands));
+
+        //View Menu
+
+        //Tools Menu
+    }
+}
