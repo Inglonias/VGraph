3c7e0a921322b1c8b26e09baf44f2f7f645f3cda
Inglonias <2125926+Inglonias@users.noreply.github.com>
2021-12-30 15:00:00 -0500

    Add documentation

--- src/drawTools/IDrawTool.cs
+++ src/drawTools/IDrawTool.cs
@@ -6,6 +6,13 @@ namespace VGraph.src.drawTools
 {
     public interface IDrawTool
     {
+        /// <summary>
+        /// Returns a list of line segments to draw onto the page.
+        /// These line segments will be drawn in the order they are stored in the array.
+        /// </summary>
+        /// <param name="start">The start point of whatever you are drawing. Interpret this however you like.</param>
+        /// <param name="end">The end point of whatever you are drawing. Interpret this however you like.</param>
+        /// <returns></returns>
         LineSegment[] DrawWithTool(SKPointI start, SKPointI end);
     }
 }
