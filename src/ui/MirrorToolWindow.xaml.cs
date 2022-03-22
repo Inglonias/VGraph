5935b34b8e9a01c4010182b0b9c7c291a438f3d9
Inglonias <2125926+Inglonias@users.noreply.github.com>
2022-03-22 09:02:01 -0400

    Added the UI for the mirror tool

---
+++ src/ui/MirrorToolWindow.xaml.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VGraph.src.ui
+{
+    /// <summary>
+    /// Interaction logic for MirrorToolWindow.xaml
+    /// </summary>
+    public partial class MirrorToolWindow : Window
+    {
+        public MirrorToolWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
