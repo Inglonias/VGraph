14e93acde7b9c5f7b71524bc196b4f5a8d255117
Inglonias <2125926+Inglonias@users.noreply.github.com>
2022-05-02 13:37:33 -0400

    Added unit tests to render the GUI windows.

---
+++ VGraphTestSuite/tests/GuiRenderTests.cs
@@ -0,0 +1,62 @@
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VGraph.src.ui;
+
+namespace VGraphTestSuite
+{
+    [TestClass]
+    public class GuiRenderTests
+    {
+        //Class blatantly stolen from https://getyourbitstogether.com/wpf-and-mstest/
+        //The above link was blatantly stolen from https://www.meziantou.net/mstest-v2-customize-test-execution.htm
+        private class WpfTestMethodAttribute : TestMethodAttribute
+        {
+            public override TestResult[] Execute(ITestMethod testMethod)
+            {
+                if (Thread.CurrentThread.GetApartmentState() == ApartmentState.STA)
+                    return Invoke(testMethod);
+
+                TestResult[] result = null;
+                var thread = new Thread(() => result = Invoke(testMethod));
+                thread.SetApartmentState(ApartmentState.STA);
+                thread.Start();
+                thread.Join();
+                return result;
+            }
+
+            private TestResult[] Invoke(ITestMethod testMethod)
+            {
+                return new[] { testMethod.Invoke(null) };
+            }
+        }
+
+        [WpfTestMethod]
+        public void TestMainWindow()
+        {
+            MainWindow mw = new MainWindow();
+            mw.Show();
+        }
+
+        [WpfTestMethod]
+        public void TestNewGridWindow()
+        {
+            NewGridWindow ngw = new NewGridWindow(false);
+            ngw.MainWindowParent = new MainWindow();
+            ngw.Show();
+        }
+
+        [WpfTestMethod]
+        public void TestMirrorGridWindow()
+        {
+            MirrorToolWindow mtw = new MirrorToolWindow();
+            mtw.Show();
+        }
+
+        [WpfTestMethod]
+        public void TestConfigWindow()
+        {
+            ConfigOptionsWindow cow = new ConfigOptionsWindow();
+            cow.Show();
+        }
+    }
+}
