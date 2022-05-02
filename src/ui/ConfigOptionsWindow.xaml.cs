c735411d1a5a766aa6eb9e821e5168fadb0f6193
Inglonias <2125926+Inglonias@users.noreply.github.com>
2022-05-02 13:18:28 -0400

    Remove unused imports from ConfigOptionsWindow.xaml.cs

--- src/ui/ConfigOptionsWindow.xaml.cs
+++ src/ui/ConfigOptionsWindow.xaml.cs
@@ -1,19 +1,7 @@
-﻿using SkiaSharp;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using VGraph.src.config;
-using VGraph.src.dataLayers;
+using SkiaSharp;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace VGraph.src.ui
 {
