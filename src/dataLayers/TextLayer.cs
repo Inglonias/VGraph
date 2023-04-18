b79b70b313236cc1aea871a6aaa32d558768c024
Inglonias <2125926+Inglonias@users.noreply.github.com>
2023-04-18 11:14:09 -0400

    Create TextLayer.cs

---
+++ src/dataLayers/TextLayer.cs
@@ -0,0 +1,34 @@
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using VGraph.src.objects;
+
+namespace VGraph.src.dataLayers
+{
+    public class TextLayer : IDataLayer
+    {
+        private bool RedrawOverride = false;
+        bool IDataLayer.DrawInExport => true;
+        public List<TextLabel> TextLabels { get; private set; } = new List<TextLabel>();
+
+        public void ForceRedraw()
+        {
+            RedrawOverride = true;
+        }
+
+        public SKBitmap GenerateLayerBitmap()
+        {
+            throw new NotImplementedException();
+        }
+
+        public SKPointI GetRenderPoint()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsRedrawRequired()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
