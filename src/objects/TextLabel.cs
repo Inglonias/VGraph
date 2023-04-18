542c4d1ed625892ac0550b4b5210ce8bdd8633a5
Inglonias <2125926+Inglonias@users.noreply.github.com>
2023-04-18 11:14:36 -0400

    Create TextLabel.cs

---
+++ src/objects/TextLabel.cs
@@ -0,0 +1,12 @@
+using SkiaSharp;
+
+namespace VGraph.src.objects
+{
+    public class TextLabel
+    {
+        SKPointI RenderPoint { get; set; }
+        string LabelText { get; set; } = "";
+        SKColor LabelColor { get; set; }
+        SKFont FontData { get; set; } = new SKFont();
+    }
+}
