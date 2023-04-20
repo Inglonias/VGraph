dfb35f6d151abc3f9f8d1e9c8e47e6a12b20231a
Inglonias <2125926+Inglonias@users.noreply.github.com>
2023-04-20 17:22:57 -0400

    Added a test class for the text tool

---
+++ VGraphTestSuite/tests/TextTests.cs
@@ -0,0 +1,28 @@
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SkiaSharp;
+using VGraph.src.config;
+using VGraph.src.dataLayers;
+using VGraph.src.drawTools;
+using VGraph.src.objects;
+using VGraphTestSuite.util;
+
+namespace VGraphTestSuite
+{
+    [TestClass]
+    public class TextTests
+    {
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            TestUtils.AssignPageData();
+        }
+
+        [TestMethod]
+        public void TestTextTool()
+        {
+            
+        }
+    }
+}
